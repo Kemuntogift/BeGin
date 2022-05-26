9e0cd6feb89464fe2c399dad518cead9c6e3726e
Kemunto Gift <giftoyugi@gmail.com>
2022-05-26 08:32:44 +0300

    add logic for the cancel button

--- app/src/main/java/com/kemunto/begin/GinDialogFragment.java
+++ app/src/main/java/com/kemunto/begin/GinDialogFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 import androidx.fragment.app.DialogFragment;
+
+import java.util.Objects;
 
 
 public class GinDialogFragment extends DialogFragment {
@@ -14,7 +17,17 @@ public class GinDialogFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_gin_dialog, container, false);
-        getDialog().setTitle("Simple Dialog");
+        Objects.requireNonNull(getDialog()).setTitle("Simple Dialog");
+
+        Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
+        Button submitButton = (Button) rootView.findViewById(R.id.submitButton);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
         return rootView;
     }
 }

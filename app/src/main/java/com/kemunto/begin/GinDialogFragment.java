2c0a94991433c15985aae3dd791865db1f1958e0
Kemunto Gift <giftoyugi@gmail.com>
2022-05-26 08:28:55 +0300

    add cancel and submit button

--- app/src/main/java/com/kemunto/begin/GinDialogFragment.java
+++ app/src/main/java/com/kemunto/begin/GinDialogFragment.java
@@ -1,18 +1,20 @@
 package com.kemunto.begin;
 
-import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class GinDialogFragment extends Dialog Fragment{
+
+import androidx.fragment.app.DialogFragment;
+
+
+public class GinDialogFragment extends DialogFragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View rootview = inflater.inflate(R.layout.fragment_gin_dialog, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_gin_dialog, container, false);
         getDialog().setTitle("Simple Dialog");
-        return rootview;
-
+        return rootView;
     }
 }
--- app/src/main/java/com/kemunto/begin/MainActivity.java
+++ app/src/main/java/com/kemunto/begin/MainActivity.java
@@ -1,14 +1,31 @@
 package com.kemunto.begin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.ginButton1) Button ginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        ginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getSupportFragmentManager();
+                GinDialogFragment moodDialogFragment = new GinDialogFragment();
+                moodDialogFragment.show(fm, "Sample Fragment");
+            }
+        });
     }
 }

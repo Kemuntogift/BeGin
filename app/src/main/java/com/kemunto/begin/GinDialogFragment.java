6996b0a02732bc6fdf5077885bf99666d495e21c
Kemunto Gift <giftoyugi@gmail.com>
2022-05-26 07:46:19 +0300

    add linear fragment layout

--- app/src/main/java/com/kemunto/begin/GinDialogFragment.java
+++ app/src/main/java/com/kemunto/begin/GinDialogFragment.java
@@ -1,4 +1,18 @@
 package com.kemunto.begin;
 
-public class GinDialogFragment {
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GinDialogFragment extends Dialog Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View rootview = inflater.inflate(R.layout.fragment_gin_dialog, container, false);
+        getDialog().setTitle("Simple Dialog");
+        return rootview;
+
+    }
 }
